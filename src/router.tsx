79615fed5f9d020348bbbe7cf3252d75c44a351d
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-05-14 20:59:32 +0300

    update: added layout to router

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+import { Layout } from './components/Layout/Layout'
 import { Characters } from './pages/Characters'
 
 const router = createBrowserRouter([
   {
-    element: <Characters />,
-    path: '/',
+    children: [
+      {
+        element: <Characters />,
+        path: '/',
+      },
+    ],
+    element: <Layout />,
   },
 ])
 
