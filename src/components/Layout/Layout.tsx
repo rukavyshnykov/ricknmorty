31da8f86bda2705f5bdb97ada6af24be1dd13fd3
rukavyshnykov <82444499+rukavyshnykov@users.noreply.github.com>
2024-05-14 21:01:30 +0300

    feature: added layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import logo from '@/assets/logo.png'
+import { Sidebar } from '@/features/Sidebar/Sidebar'
+import { Characters } from '@/pages/Characters'
+export const Layout = () => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  return (
+    <div>
+      <div className={'flex items-center bg-stone-700 gap-2.5 p-5'}>
+        <Sidebar changeModalState={setOpen} open={open} />
+        <img className={'w-40'} src={logo} />
+      </div>
+      <div className={'mb-8 mt-8 w-full'}>
+        <div className={'flex content-center mx-auto my-0 max-w-7xl'}>
+          <Characters />
+        </div>
+      </div>
+    </div>
+  )
+}
